b53ce01b3bc320b5c11fe7659125c4c720943f1b
Davide Aversa <thek3nger@gmail.com>
2014-01-22 20:02:38 +0100

    Add some unclear code that can be removed in future commits

---
+++ Assets/BHTRawTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using RoomOfRequirement.AI.BehaviorTree;
+using RoomOfRequirement.AI.Data;
+
+public class BHTRawTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Task root = new Sequence(
+			new PrintTask().SetMessage("Go to door"),
+			new PrintTask().SetMessage("Knock knock"),
+			new UntilFail(new Limit(new PrintTask().SetMessage("Waiting..."),10)));
+		root.Run(new HierarchicalBlackboard());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class PrintTask : Task {
+
+	string message;
+
+	public Task SetMessage(string msg) {
+		message = msg;
+		return this;
+	}
+
+	public override TaskReturnValue Run (HierarchicalBlackboard blackboard) {
+		Debug.Log(message);
+		return TaskReturnValue.SUCCESS;
+	}
+}
